001130d50baa982c39a86b00742efc66f5ade034
Caserla <04102198q@163.com>
2016-06-30 15:41:35 +0800

    logout

--- app/src/main/java/com/wangdiaozhu/qingweibo/pager/ProfilePager.java
+++ app/src/main/java/com/wangdiaozhu/qingweibo/pager/ProfilePager.java
@@ -1,15 +1,26 @@
 package com.wangdiaozhu.qingweibo.pager;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.lidroid.xutils.BitmapUtils;
+import com.sina.weibo.sdk.auth.Oauth2AccessToken;
+import com.sina.weibo.sdk.exception.WeiboException;
+import com.sina.weibo.sdk.net.RequestListener;
+import com.sina.weibo.sdk.openapi.LogoutAPI;
+import com.sina.weibo.sdk.openapi.models.ErrorInfo;
 import com.sina.weibo.sdk.openapi.models.User;
+import com.wangdiaozhu.qingweibo.Constants.AccessTokenKeeper;
+import com.wangdiaozhu.qingweibo.Constants.Constants;
 import com.wangdiaozhu.qingweibo.R;
 import com.wangdiaozhu.qingweibo.activity.MainActivity;
+import com.wangdiaozhu.qingweibo.activity.SplashActivity;
 import com.wangdiaozhu.qingweibo.base.BasePager;
 import com.wangdiaozhu.qingweibo.fragment.ContentFragment;
+import com.wangdiaozhu.qingweibo.utils.PrefUtils;
 import com.wangdiaozhu.qingweibo.view.CircleImageView;
 
 /**
@@ -24,8 +35,12 @@ public class ProfilePager extends BasePager {
     TextView tv_status_count;
     TextView tv_focus_count;
     TextView tv_funs_count;
+    private Oauth2AccessToken mAccessToken;
     private User user;
     public BitmapUtils mBitmapUtils;
+    private Button btn_logout;
+    private LogoutAPI mLogout;
+    boolean isregisted;
     public ProfilePager(Activity activity) {
         super(activity);
             mBitmapUtils = new BitmapUtils(mActivity);
@@ -48,31 +63,52 @@ public class ProfilePager extends BasePager {
         tv_status_count = (TextView) mProfilePager.findViewById(R.id.tv_status_count);
         tv_focus_count = (TextView) mProfilePager.findViewById(R.id.tv_focus_count);
         tv_funs_count = (TextView) mProfilePager.findViewById(R.id.tv_funs_count);
-
+        btn_logout = (Button) mProfilePager.findViewById(R.id.btn_logout);
         user = getUserInfo();
         tv_user_name.setText(user.screen_name);
-         mBitmapUtils.display(clv_profile_avatar,user.avatar_hd);
-        tv_introduction.setText("简介: "+user.description);
+         mBitmapUtils.display(clv_profile_avatar, user.avatar_hd);
+        tv_introduction.setText("简介: " + user.description);
         tv_status_count.setText(user.statuses_count+"");
         tv_focus_count.setText(user.friends_count+"");
         tv_funs_count.setText(user.followers_count+"");
+        mAccessToken = AccessTokenKeeper.readAccessToken(mActivity);
+        mLogout = new LogoutAPI(mActivity, Constants.APP_KEY,mAccessToken);
+        btn_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              mLogout.logout(mListener);
+                Intent intent = new Intent(mActivity, SplashActivity.class);
+                mActivity.startActivity(intent);
+
+                isregisted = false;
+                PrefUtils.putBoolean("isRegisted",isregisted,mActivity);
+                 mActivity.finish();
+            }
+        });
+
+
     }
     private User getUserInfo(){
-
-
         MainActivity mainActivity = (MainActivity) mActivity;
         ContentFragment fragment =  mainActivity.getContentFragment();
         User user = fragment.getUser();
         if (user != null) {
-
             return user;
-
         }else {
-
             System.out.println("获取数据失败");
         }
         return null;
     }
+    private RequestListener mListener = new RequestListener() {
+        @Override
+        public void onComplete(String response) {
+
+        }
+        @Override
+        public void onWeiboException(WeiboException e) {
+            ErrorInfo info = ErrorInfo.parse(e.getMessage());
+        }
+    };
 
 
 }
